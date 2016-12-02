4e25dab7c919394d08fad6ffe1c463336cd6a493
Thomas Lee <lisihan969@gmail.com>
2016-12-02 19:44:43 +0800

    conv_encoder tested

--- src/conv_encoder.v
+++ src/conv_encoder.v
@@ -1,11 +1,9 @@
-`timescale 1ns / 1ps
-
 module conv_encoder(
-    input  clk,
-    input  clk_div2,
-    input  x,
-    input  rst_n,
-    output reg y,
+    input clk,
+    input clk_div2,
+    input x,
+    input rst_n,
+    output reg y
 );
 
 reg [2:0] regs;
@@ -13,22 +11,26 @@ reg status;
 
 always @(posedge clk_div2) begin
     if (!rst_n) begin
-        regs <=  0;
+        regs <= 0;
     end else begin
         regs <= {x, regs[2:1]};
     end
 end
 
-always @(posedge clk_div) begin
+always @(posedge clk) begin
     if (!rst_n) begin
         status <=  0;
+        y <= 0;
     end else begin
-        if (status) begin
-            y <= regs[2] ^ regs[1] ^ regs[0];
-        end else begin
-            y <= regs[2] ^ regs[0];
-        end
         status <= ~status;
+    end
+end
+
+always @(*) begin
+    if (status) begin
+        y <= regs[2] ^ regs[1] ^ regs[0];
+    end else begin
+        y <= regs[2] ^ regs[0];
     end
 end
 
