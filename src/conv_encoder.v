1450bf45d59e521812d5ec5ad9df3df8ec38bd9c
Thomas Lee <lisihan969@gmail.com>
2016-12-02 19:08:45 +0800

    Reindent

--- src/conv_encoder.v
+++ src/conv_encoder.v
@@ -1,18 +1,18 @@
 `timescale 1ns / 1ps
 
 module conv_encoder(
-  input  clk,
-  input  clk_div2,
-  input  x,
-  input  rst_n,
-  output reg y,
+    input  clk,
+    input  clk_div2,
+    input  x,
+    input  rst_n,
+    output reg y,
 );
 
 reg [2:0] regs;
 reg status;
 
 always @(posedge clk_div2) begin
-   if (!rst_n) begin
+    if (!rst_n) begin
         regs <=  0;
     end else begin
         regs <= {x, regs[2:1]};
@@ -20,7 +20,7 @@ always @(posedge clk_div2) begin
 end
 
 always @(posedge clk_div) begin
-   if (!rst_n) begin
+    if (!rst_n) begin
         status <=  0;
     end else begin
         if (status) begin
